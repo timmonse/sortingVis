d6cf5aa37b31a719d88b0d1a27ee9e85f5bba44d
Ismael <iduran@iastate.edu>
2020-04-01 18:02:44 -0500

    Functionality of sort page

--- sortingVis/resources/js/sortMenu.js
+++ sortingVis/resources/js/sortMenu.js
@@ -3,37 +3,74 @@ var sorts = {
     Model: [
         {
             "name": "Bubble Sort",
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png"
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
         },
         {
             "name": "Bubble Sort",
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png"
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
         },
         {
             "name": "Bubble Sort",
-            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png"
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
+        },
+        {
+            "name": "Bubble Sort",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
+        },
+        {
+            "name": "Bubble Sort",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
+        },
+        {
+            "name": "Bubble Sort",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
+        },
+        {
+            "name": "Bubble Sort",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
+        },
+        {
+            "name": "Bubble Sort",
+            "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
+            "page": "mainMenu.html"
         },
 
     ],
 
     // this function will display all algorithms on the page using model list
     displaySortCard: function () {
-        var displayArea = document.getElementById('sorts-display');
-        for (var i = 0; i < sorts.Model.length; i++) {
-            var entry = document.createElement('div');
-            entry.className = 'card';
-            var image = document.createElement('img');
-            image.src = sorts.Model[i].image;
-            image.className = 'card-image';
-            var container = document.createElement('div');
-            var name = document.createElement('p');
-            name.className = 'card-sort-name-font';
-            name.innerHTML = sorts.Model[i].name;
-            container.appendChild(name);
-            entry.appendChild(image);
-            entry.appendChild(container);
-            displayArea.appendChild(entry)
+        let displayArea = document.getElementById('sorts-display');
+        for (let i = 0; i < sorts.Model.length; i++) {
+            let entry = sorts.createCard(sorts.Model[i]);
+            displayArea.appendChild(entry);
         }
     },
 
-}
+    createCard: function (sort) {
+        let entry = document.createElement('a');
+        entry.className = 'card draw-border';
+        entry.href = sort.page;
+        let image = document.createElement('img');
+        image.src = sort.image;
+        image.className = 'card-image';
+        let container = document.createElement('div');
+        container.className='card-container'
+        let name = document.createElement('p');
+        name.className = 'card-sort-name-font';
+        name.innerHTML = sort.name;
+        container.appendChild(name);
+        entry.appendChild(image);
+        entry.appendChild(container);
+        return entry;
+    }
+
+
+
+};
