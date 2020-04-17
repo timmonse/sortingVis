edc5eb15bd574bea5678dd48e4423613de93591e
Ismael <duran.ismael30@gmail.com>
2020-04-17 01:26:58 -0500

    store sort name on local storage

--- sortingVis/resources/js/sortMenu.js
+++ sortingVis/resources/js/sortMenu.js
@@ -4,17 +4,20 @@ var sorts = {
         {
             "name": "Bubble Sort",
             "image": "https://i2.wp.com/abovethecrowd.com/wp-content/uploads/2014/01/bubbles-water-transparent-design.jpg?resize=1024%2C768",
-            "page": "sorting.html"
+            "page": "sorting.html",
+            "value" : "BubbleSort",
         },
         {
             "name": "Selection Sort",
             "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
-            "page": "sorting.html"
+            "page": "sorting.html",
+            "value" : "SelectionSort",
         },
         {
             "name": "Merge Sort",
             "image": "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Bubblesort-edited-color.svg/220px-Bubblesort-edited-color.svg.png",
-            "page": "sorting.html"
+            "page": "sorting.html",
+            "value" : "MergeSort",
         },
 
     ],
@@ -33,6 +36,7 @@ var sorts = {
         entry.className = 'card draw-border';
 
         entry.addEventListener('click', function () {
+            localStorage.setItem("type", sort.value );
             window.document.location = sort.page;
         });
 
