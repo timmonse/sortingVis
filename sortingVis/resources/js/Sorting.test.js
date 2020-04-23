78f99212ac04a37fe7a51ea13c10419eb56bb783
Ty Wallis <tjwallis@iastate.edu>
2020-04-23 12:08:52 -0500

    Insersion sort test
    program passes test

--- sortingVis/resources/js/Sorting.test.js
+++ sortingVis/resources/js/Sorting.test.js
@@ -15,3 +15,21 @@ describe('Bubble Sort Test', () => {
         expect(data).toEqual(expected);
     });
 });
+
+describe('Inserstion Sort Test', () => {
+    const Sorting = require('./Sorting');
+    const data = [2,4,7,1,3,0,8,9,6,5];
+    const showArray = jest.fn();
+    const checkPause = jest.fn();
+
+    const expected = [0,1,2,3,4,5,6,7,8,9];
+
+    it('Function should call Draw() function, call Wait() function, and be sorted after returning.', async () => {
+        await Sorting.insertion_sort(data, showArray, checkPause);
+
+        expect(showArray).toBeCalled();
+        expect(checkPause).toBeCalled();
+
+        expect(data).toEqual(expected);
+    });
+});
