4b676881749189d7b125bdd6f8aeadb013845909
Ismael <duran.ismael30@gmail.com>
2020-04-30 15:13:14 -0500

    sort menu has audio

--- sortingVis/resources/js/sortMenu.js
+++ sortingVis/resources/js/sortMenu.js
@@ -71,5 +71,43 @@ var sorts = {
         entry.appendChild(image);
         entry.appendChild(container);
         return entry;
+    },
+
+    initPage: function () {
+        let music = localStorage.getItem("music");
+        var isPlaying = false;
+        if(music == null){
+            isPlaying =false;
+        }
+        else if (music == "true"){
+            isPlaying= true;
+        }
+
+        var audio;
+        audio = new Audio("resources/music/jazzyfrenchy.mp3");
+        audio.loop = true;
+        let musicElement = document.getElementById("musicState");
+        if(isPlaying){
+            audio.play();
+            musicElement.className = "fa fa-volume-off fa-2x musicIcon ";
+            localStorage.setItem("music", "true");
+        }
+
+
+        musicElement.addEventListener("click", function () {
+            if(!isPlaying){
+                audio.play()
+                musicElement.className = "fa fa-volume-off fa-2x musicIcon ";
+                isPlaying = !isPlaying;
+                localStorage.setItem("music", "true");
+            }
+            else{
+                audio.pause();
+                isPlaying = !isPlaying;
+                musicElement.className = "fa fa-music fa-2x musicIcon";
+                localStorage.setItem("music", "false");
+            }
+        });
+
     }
 };
